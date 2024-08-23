7c9060b40b65f2549fa88bbad6a06a3a541d96db
hkaranam <hkaranam241@gmail.com>
2024-08-23 05:58:41 -0700

    second commit

--- 08-shipping.sh
+++ 08-shipping.sh
@@ -4,6 +4,7 @@ USERID=$(id -u )
 TIMESTAMP=$(date +%F-%H-%M-%S)
 Script_Name=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$Script_Name-$TIMESTAMP.log
+DIRECTORY=/app
 
 if [ $USERID -eq 0 ]
 then
@@ -28,10 +29,16 @@ validate $? "installing maven"
 useradd roboshop &>>$LOGFILE
 validate $? "Creating roboshop user"
 
-mkdir /app &>>$LOGFILE
-validate $? "Creating App folder"
+if [ -d "$DIRECTORY" ]; 
+then
+    echo "removing the directory"
+    rm -rf $DIRECTORY
+    mkdir -p  $DIRECTORY
+else
+    mkdir -p $DIRECTORY
+fi
 
-curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip &>>$LOGFILE
+curl  -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip &>>$LOGFILE
 validate $? "downloading shiiping code"
 
 cd /app &>>$LOGFILE
@@ -40,16 +47,13 @@ validate $? "changing the app folder"
 unzip /tmp/shipping.zip &>>$LOGFILE
 validate $? "unzip the shiiping code"
 
-cd /app &>>$LOGFILE
-validate $? "changing the folder"
-
 mvn clean package &>>$LOGFILE
 Validate $? "Clean package"
 
 mv target/shipping-1.0.jar shipping.jar &>>$LOGFILE
 validate $? "Changing shipping jar file name"
 
-cp shipping.service /etc/systemd/system/shipping.service &>>$LOGFILE
+cp /home/ec2-user/Roboshop_linux/shipping.service /etc/systemd/system/shipping.service &>>$LOGFILE
 validate $? "Copy the service file to systemd"
 
 systemctl daemon-reload &>>$LOGFILE
